426959975ca2e4ff01ded18cac891bb02ea25da9
SunsetMP <sunsetmp@local>
2026-09-16 04:35:37 +0300

    Add 14 addon vehicles: 8 addon (vd_tenfrally, comets2cup, sentinel_rts, vd_buffalo4, elegysa, kurumac, d7cyp, H4RxST2) in sunset_addon_vehicles + 6 DLC-format (toreroxoc, mxls, schlagenstr, pentro, sultan2c, algoschafter) in sunset_addon_vehicles_dlc. Combined meta files, unique kit IDs verified, no name conflicts with existing vehicles. ~304MB total.

---
+++ resources/[sunset]/sunset_addon_vehicles/fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name 'sunset_addon_vehicles'
+author 'SunsetMP'
+description 'Addon vehicles: vd_tenfrally, comets2cup, sentinel_rts, vd_buffalo4, elegysa, kurumac, d7cyp, H4RxST2'
+version '1.0.0'
+
+files {
+    'vehicles.meta',
+    'handling.meta',
+    'carvariations.meta',
+    'carcols.meta',
+}
+
+data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
+data_file 'HANDLING_FILE' 'handling.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
+data_file 'CARCOLS_FILE' 'carcols.meta'
---
+++ resources/[sunset]/sunset_addon_vehicles_dlc/fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name 'sunset_addon_vehicles_dlc'
+author 'SunsetMP'
+description 'Addon DLC vehicles (toreroxoc, mxls, schlagenstr, pentro, sultan2c, algoschafter)'
+version '1.0.0'
+
+files {
+    'stream/*.rpf',
+}
+
+    data_file 'DLC_ITYP_REQUEST' 'stream/toreroxoc.rpf'
+    data_file 'DLC_ITYP_REQUEST' 'stream/mxls.rpf'
+    data_file 'DLC_ITYP_REQUEST' 'stream/schlagenstr.rpf'
+    data_file 'DLC_ITYP_REQUEST' 'stream/pentro.rpf'
+    data_file 'DLC_ITYP_REQUEST' 'stream/sultan2c.rpf'
+    data_file 'DLC_ITYP_REQUEST' 'stream/algoschafter.rpf'
