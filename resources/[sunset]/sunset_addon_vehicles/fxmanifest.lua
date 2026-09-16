e394d334d5c26f6c96b1a6e888f10ac8e9e99718
SunsetMP <sunsetmp@local>
2026-09-16 06:42:01 +0300

    Fix addon vehicles: rebuild meta files with proper XML parser (nested Item tags handled correctly, all 4 metas well-formed); DLC RPFs moved from stream/ to resource root with explicit DLC_ITYP_REQUEST entries (was wildcard stream/*.rpf which is invalid); 8 addon vehicles (vd_tenfrally, cometcup, sentinel_rts, vd_buffalo4, elegysa, kurumac, d7cyp, H4RxST2) + 6 DLC vehicles (toreroxoc, mxls, schlagenstr, pentro, sultan2c, algoschafter); both resources in server.cfg

--- resources/[sunset]/sunset_addon_vehicles/fxmanifest.lua
+++ resources/[sunset]/sunset_addon_vehicles/fxmanifest.lua
@@ -3,7 +3,7 @@ game 'gta5'
 
 name 'sunset_addon_vehicles'
 author 'SunsetMP'
-description 'Addon vehicles: vd_tenfrally, comets2cup, sentinel_rts, vd_buffalo4, elegysa, kurumac, d7cyp, H4RxST2'
+description 'Addon vehicles: vd_tenfrally, cometcup, sentinel_rts, vd_buffalo4, elegysa, kurumac, d7cyp, H4RxST2'
 version '1.0.0'
 
 files {
--- resources/[sunset]/sunset_addon_vehicles_dlc/fxmanifest.lua
+++ resources/[sunset]/sunset_addon_vehicles_dlc/fxmanifest.lua
@@ -3,16 +3,21 @@ game 'gta5'
 
 name 'sunset_addon_vehicles_dlc'
 author 'SunsetMP'
-description 'Addon DLC vehicles (toreroxoc, mxls, schlagenstr, pentro, sultan2c, algoschafter)'
+description 'Addon DLC vehicles: toreroxoc, mxls, schlagenstr, pentro, sultan2c, algoschafter'
 version '1.0.0'
 
 files {
-    'stream/*.rpf',
+    'toreroxoc.rpf',
+    'mxls.rpf',
+    'schlagenstr.rpf',
+    'pentro.rpf',
+    'sultan2c.rpf',
+    'algoschafter.rpf',
 }
 
-    data_file 'DLC_ITYP_REQUEST' 'stream/toreroxoc.rpf'
-    data_file 'DLC_ITYP_REQUEST' 'stream/mxls.rpf'
-    data_file 'DLC_ITYP_REQUEST' 'stream/schlagenstr.rpf'
-    data_file 'DLC_ITYP_REQUEST' 'stream/pentro.rpf'
-    data_file 'DLC_ITYP_REQUEST' 'stream/sultan2c.rpf'
-    data_file 'DLC_ITYP_REQUEST' 'stream/algoschafter.rpf'
+data_file 'DLC_ITYP_REQUEST' 'toreroxoc.rpf'
+data_file 'DLC_ITYP_REQUEST' 'mxls.rpf'
+data_file 'DLC_ITYP_REQUEST' 'schlagenstr.rpf'
+data_file 'DLC_ITYP_REQUEST' 'pentro.rpf'
+data_file 'DLC_ITYP_REQUEST' 'sultan2c.rpf'
+data_file 'DLC_ITYP_REQUEST' 'algoschafter.rpf'
