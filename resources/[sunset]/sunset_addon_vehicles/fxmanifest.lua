615c6c9a14194f54357397a8cab6fb7d681a2b1c
SunsetMP <sunsetmp@local>
2026-09-16 14:37:19 +0300

    fix(vehicles): keep only tempesta2 and bmw models (sentinel_rts, d7cyp)

--- resources/[sunset]/sunset_addon_vehicles/fxmanifest.lua
+++ resources/[sunset]/sunset_addon_vehicles/fxmanifest.lua
@@ -3,7 +3,7 @@ game 'gta5'
 
 name 'sunset_addon_vehicles'
 author 'SunsetMP'
-description 'Addon vehicles pack: civilian super/sports/sedans + BCSO Sheriff emergency fleet'
+description 'Addon vehicles: tempesta2, sentinel_rts, d7cyp'
 version '1.0.0'
 
 files {
@@ -11,11 +11,9 @@ files {
     'handling.meta',
     'carvariations.meta',
     'carcols.meta',
-    'vehiclelayouts.meta',
 }
 
 data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
 data_file 'HANDLING_FILE' 'handling.meta'
 data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
 data_file 'CARCOLS_FILE' 'carcols.meta'
-data_file 'VEHICLE_LAYOUTS_FILE' 'vehiclelayouts.meta'
--- sql/53-addon-vehicles.sql
+++ sql/53-addon-vehicles.sql
@@ -1,28 +1,10 @@
--- Migration 53: Addon Vehicles Dealership Catalog Seed
+-- Migration 53: Addon Vehicles Dealership Catalog Seed (Tempesta2 & BMWs)
 
 INSERT IGNORE INTO `dealership_vehicles`
     (`model`, `label`, `brand`, `category`, `price`, `stock`, `available`, `test_drive_enabled`, `display_order`)
 VALUES
-    ('vd_tenfrally', '10F Rally Custom', 'Obey', 'sports', 185000, 10, 1, 1, 10),
-    ('cometcup', 'Comet Cup Edition', 'Pfister', 'sports', 210000, 8, 1, 1, 11),
-    ('sentinel_rts', 'Sentinel RTS Track', 'Ubermacht', 'coupe', 145000, 12, 1, 1, 12),
-    ('vd_buffalo4', 'Buffalo STX Custom', 'Bravado', 'muscle', 165000, 15, 1, 1, 13),
-    ('elegysa', 'Elegy SA Spec', 'Annis', 'sports', 135000, 10, 1, 1, 14),
-    ('kurumac', 'Kuruma Custom', 'Karin', 'sports', 120000, 15, 1, 1, 15),
-    ('d7cyp', 'Cypher GTS Spec', 'Ubermacht', 'super', 380000, 5, 1, 1, 16),
-    ('H4RxST2', 'Harx ST2 GT', 'Pfister', 'sports', 195000, 8, 1, 1, 17),
-    ('toreroxoc', 'Torero XO Custom', 'Pegassi', 'super', 420000, 5, 1, 1, 18),
-    ('tempesta2', 'Tempesta Widebody', 'Pegassi', 'super', 360000, 6, 1, 1, 19),
-    ('mxls', 'Monarch XLS', 'Benefactor', 'sedans', 110000, 15, 1, 1, 20),
-    ('schlagenstr', 'Schlagen STR', 'Benefactor', 'sports', 175000, 10, 1, 1, 21),
-    ('pentro', 'Pentro Classic', 'Maibatsu', 'sports', 95000, 15, 1, 1, 22),
-    ('pentro2', 'Pentro Type II', 'Maibatsu', 'sports', 105000, 12, 1, 1, 23),
-    ('pentro3', 'Pentro GT Coupe', 'Maibatsu', 'coupe', 115000, 12, 1, 1, 24),
-    ('pentrogpr', 'Pentro GPR', 'Maibatsu', 'super', 240000, 8, 1, 1, 25),
-    ('pentrogpr2', 'Pentro GPR Evo', 'Maibatsu', 'super', 260000, 8, 1, 1, 26),
-    ('scheisser', 'Scheisser Sport', 'Benefactor', 'sedans', 85000, 15, 1, 1, 27),
-    ('severo', 'Severo Super V12', 'Pegassi', 'super', 450000, 5, 1, 1, 28),
-    ('algoschafter', 'Schafter V12 LWB', 'Benefactor', 'sedans', 125000, 15, 1, 1, 29),
-    ('hachurac', 'Hachura Custom', 'Vulcar', 'sports', 130000, 12, 1, 1, 30),
-    ('sultan2c', 'Sultan Classic Custom', 'Karin', 'sports', 140000, 12, 1, 1, 31);
+    ('tempesta2', 'Tempesta Widebody', 'Pegassi', 'super', 360000, 6, 1, 1, 10),
+    ('sentinel_rts', 'Sentinel RTS Track', 'Ubermacht', 'coupe', 145000, 12, 1, 1, 11),
+    ('d7cyp', 'Cypher GTS Spec', 'Ubermacht', 'super', 380000, 5, 1, 1, 12);
+
 
